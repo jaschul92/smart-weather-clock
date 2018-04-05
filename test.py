d407424cfeda603110fd9e57cac9bf0b2d65594d
James Schulman <jaschul@bu.edu>
2018-04-05 13:43:52 -0400

    changed weather api provider from openweather to darksky

--- test.py
+++ test.py
@@ -6,48 +6,48 @@ import sched, time
 from threading import Thread, Event
 #from Tkinter import *
 
-WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+WEATHER_BASE_URL = 'https://api.darksky.net/forecast/'
 GEOLOCATION_BASE_URL = 'http://freegeoip.net/json'
-API_KEY = str(os.environ.get('WEATHER_API_KEY'))
+API_KEY = '0de17eb41ac7bb9b6a09b74245da3039'
 
 class Weather():
    def __init__(self):
-      self.city = ''
+      self.forecast = ''
       self.temp = ''
-      self.min_temp = ''
-      self.max_temp = ''
-      self.cloud = ''
+      self.cloud_condition = ''
       self.get_weather()
 
    def get_weather(self):
       try:
-         lat,lon = get_location()
-         request_url = WEATHER_BASE_URL+'?lat='+lat+'&lon='+lon+'&APPID='+API_KEY
+         lat, lon = self.get_location()
+         request_url = WEATHER_BASE_URL+API_KEY+'/'+lat+','+lon
          resp = requests.get(request_url)
          data = json.loads(resp.text)
-         print(data)
-         
-         city = data['name']
-         weather_data = data['weather']
-         cloud_condition = weather_data[0]['main']
-         temp_data = data['main']
-         temp = kelvin_to_farenheit(int(temp_data['temp']))
-         min_temp = kelvin_to_farenheit(int(temp_data['temp_min']))
-         max_temp = kelvin_to_farenheit(int(temp_data['temp_max']))
+         curr_temp = str(int(data['currently']['temperature']))+u'\N{DEGREE SIGN}'
+         curr_cloud_condition = data['currently']['summary']
+         curr_forecast = data['hourly']['summary']
+         curr_icon = data['currently']['icon']
 
-         if self.city != city:
-            self.city = city
-         if self.cloud != cloud_condition:
-            self.cloud = cloud_condition
-         if self.temp != str(temp):
-            self.temp = str(temp)
-         if self.min_temp != str(min_temp):
-            self.min_temp = str(min_temp)
-         if self.max_temp != str(max_temp):
-            self.max_temp = str(max_temp)
+         if curr_temp != self.temp:
+            self.temp = curr_temp
+         if curr_cloud_condition != self.cloud_condition:
+            self.cloud_condition = curr_cloud_condition
+         if curr_forecast != self.forecast:
+            self.forecast = curr_forecast
 
       except Exception as e:
          print("Error: %s. Cannot get weather" %e)
+
+   def get_location(self):
+      try:
+         r = requests.get(GEOLOCATION_BASE_URL)
+         j = json.loads(r.text)
+         lat = str(j['latitude'])
+         lon = str(j['longitude'])
+         return lat,lon
+
+      except Exception as e:
+         print("Errro %s. Cannot get current location" %e)
       
 class Window():
    def __init__(self):
@@ -76,15 +76,10 @@ class MyThread(Thread):
    def run(self):
       while not self.stopped.wait(5.0):
          w = Weather()
-
-def get_location():
-   r = requests.get(GEOLOCATION_BASE_URL)
-   j = json.loads(r.text)
-   lat = str(j['latitude'])
-   lon = str(j['longitude'])
-
-   return lat,lon
-
+         print(w.temp)
+         print(w.cloud_condition)
+         print(w.forecast)
+         
 def kelvin_to_farenheit(k_temp):
    return int((k_temp - 273.15)*1.8 + 32.00)
 
@@ -97,6 +92,5 @@ def main():
    #win = Window()
    #win.tk.mainloop()
    
-     
 if __name__ == "__main__":
     main()
